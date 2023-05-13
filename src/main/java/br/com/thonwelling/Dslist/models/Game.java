78ff66743cfda2be02433d2e179f4e558199d70b
THONWELLING <thonwellingdani13@gmail.com>
2023-05-12 23:34:35 -0300

    properties, Model Game e Mapeamento

---
+++ src/main/java/br/com/thonwelling/Dslist/models/Game.java
@@ -0,0 +1,121 @@
+package br.com.thonwelling.Dslist.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String title;
+  @Column(name = "game_year")
+  private Integer year;
+  private String genre;
+  private String platforms;
+  private Double score;
+  private String imgUrl;
+  private String shortDescription;
+  private String longDescription;
+
+  public Game() {}
+
+  public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+    this.id = id;
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+    this.platforms = platforms;
+    this.score = score;
+    this.imgUrl = imgUrl;
+    this.shortDescription = shortDescription;
+    this.longDescription = longDescription;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+
+  public String getPlatforms() {
+    return platforms;
+  }
+
+  public void setPlatforms(String platforms) {
+    this.platforms = platforms;
+  }
+
+  public Double getScore() {
+    return score;
+  }
+
+  public void setScore(Double score) {
+    this.score = score;
+  }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
+  }
+
+  public String getShortDescription() {
+    return shortDescription;
+  }
+
+  public void setShortDescription(String shortDescription) {
+    this.shortDescription = shortDescription;
+  }
+
+  public String getLongDescription() {
+    return longDescription;
+  }
+
+  public void setLongDescription(String longDescription) {
+    this.longDescription = longDescription;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Game game)) return false;
+    return Objects.equals(getId(), game.getId()) && Objects.equals(getTitle(), game.getTitle()) && Objects.equals(getYear(), game.getYear()) && Objects.equals(getGenre(), game.getGenre()) && Objects.equals(getPlatforms(), game.getPlatforms()) && Objects.equals(getScore(), game.getScore()) && Objects.equals(getImgUrl(), game.getImgUrl()) && Objects.equals(getShortDescription(), game.getShortDescription()) && Objects.equals(getLongDescription(), game.getLongDescription());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getTitle(), getYear(), getGenre(), getPlatforms(), getScore(), getImgUrl(), getShortDescription(), getLongDescription());
+  }
+}
