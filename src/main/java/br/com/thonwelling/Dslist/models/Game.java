651974f8c96699a71a95a0eb7176d3c13a29c5ad
THONWELLING <thonwellingdani13@gmail.com>
2023-05-13 00:05:03 -0300

    Populando banco de testes

--- src/main/java/br/com/thonwelling/Dslist/models/Game.java
+++ src/main/java/br/com/thonwelling/Dslist/models/Game.java
@@ -18,7 +18,9 @@ public class Game {
   private String platforms;
   private Double score;
   private String imgUrl;
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
   public Game() {}
