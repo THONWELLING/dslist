f724fe4eb343450d9895d81244c3cdeefa604b2c
THONWELLING <thonwellingdani13@gmail.com>
2023-05-12 23:08:09 -0300

    Project created

---
+++ src/main/java/br/com/thonwelling/Dslist/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.thonwelling.Dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thonwelling/Dslist/StartUpTests.java
@@ -0,0 +1,13 @@
+package br.com.thonwelling.Dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
